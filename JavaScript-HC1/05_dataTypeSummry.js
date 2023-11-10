1a41ad97e0784978c6609d2ac38ce7d115ff0d93
adity12 <68803969+adity12@users.noreply.github.com>
2023-11-10 17:41:26 +0000

    05_dataTypeSummry.js

---
+++ JavaScript-HC1/05_dataTypeSummry.js
@@ -0,0 +1,11 @@
+// 1.) primitive datatype
+
+// 7types : String, Number, boolean, null, undefined, symbol, BigInt
+
+
+
+
+
+// 2.) reference (non Primitve DataTypes)
+
+// types : Array, objects, Functions
---
+++ JavaScript-HC1/06_String.js
@@ -0,0 +1,7 @@
+const name = "ADITYA"
+const repoCount = 36
+
+// console.log(name + repoCount);
+// String interpulatation (using backticks(``)to create placeholder)
+// modern way of writing
+console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);
